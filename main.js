1847611ffa3558cfa9610a13aa80fd6a397ddc66
unknown <support@vaaptech.com>
2019-05-18 15:17:18 +0530

    added comma for numbers and fixed floating decimal to two digits

--- main.js
+++ main.js
@@ -8,6 +8,23 @@ const enterAmount = document.getElementById('enter-amount');
 const enterInterest = document.getElementById('enter-interest');
 const enterMonths = document.getElementById('enter-months');
 
+function localeString(x, sep, grp) {
+    var sx = ('' + x).split('.'),
+        s = '',
+        i, j;
+    sep || (sep = ','); // default seperator
+    grp || grp === 0 || (grp = 3); // default grouping
+    i = sx[0].length;
+    while (i > grp) {
+        j = i - grp;
+        s = sep + sx[0].slice(j, i) + s;
+        i = j;
+    }
+    s = sx[0].slice(0, i) + s;
+    sx[0] = s;
+    return sx.join('.');
+}
+
 let calculateEMI = () => {
     console.log('clicked');
     let amountValue = amount.value;
@@ -16,12 +33,15 @@ let calculateEMI = () => {
     let interestRate = interestValue * (monthsValue / 100);
 
     let EMI = [amountValue * interestRate * Math.pow((1 + interestRate), monthsValue)] / [Math.pow((1 + interestRate), (monthsValue - 1))];
-    enterAmount.innerHTML = amountValue;
+    enterAmount.innerHTML = localeString(amountValue);
     enterInterest.innerHTML = interestValue;
     enterMonths.innerHTML = monthsValue;
-    finalValue.innerHTML = EMI;
+    finalValue.innerHTML = localeString(EMI.toFixed(2));
 }
 
 amount.addEventListener('click', calculateEMI);
 interest.addEventListener('click', calculateEMI);
 months.addEventListener('click', calculateEMI);
+amount.addEventListener('change', calculateEMI);
+interest.addEventListener('change', calculateEMI);
+months.addEventListener('change', calculateEMI);
